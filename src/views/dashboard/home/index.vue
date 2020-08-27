eb9f9cbb843aadaabc2731c2d84d3c11c4860f1a
Nitish Kumar Singh <nitishk72@gmail.com>
2020-08-27 17:12:19 +0530

    done

--- src/views/dashboard/home/index.vue
+++ src/views/dashboard/home/index.vue
@@ -1,127 +1,132 @@
 <template>
-    <div class="container">
-        <progress class="pure-material-progress-linear" v-if="loading" />
-        <div class="my-4" v-if="!loading">
-            <div class=" row">
-                <div class="col-lg-6 col-sm-12 pb-2" v-if="Object.keys(projects.data).length == 3" lg="6" sm="12"
-                    cols="sm">
-                    <div class="card" style="min-height:150px">
-                        <div class="card-header">
-                            <div style="font-size: 20px;">
-                                Upgrade to Create More Projects
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row no-gutters">
-                                <div class="col-auto">
-                                    <div class="cyan fill-height">&nbsp;</div>
-                                </div>
-                                <div class="col pa-3 py-4">
-                                    <div>
-                                        We can't offer more for free, please upgrade to get more.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-footer">
-                            <router-link class="btn btn-warning" to="upgrade">Upgrade Account</router-link>
-                        </div>
-
-                    </div>
-
+  <div class="container">
+    <progress class="pure-material-progress-linear" v-if="loading" />
+    <div class="my-4" v-if="!loading">
+      <div class=" row">
+        <div
+          class="col-lg-6 col-sm-12 pb-2"
+          v-if="Object.keys(projects.data).length == 3"
+          lg="6"
+          sm="12"
+          cols="sm"
+        >
+          <div class="card" style="min-height:150px">
+            <div class="card-header">
+              <div style="font-size: 20px;">
+                Upgrade to Create More Projects
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <div class="cyan fill-height">&nbsp;</div>
                 </div>
-
-                <div class="col-lg-6 col-sm-12 pb-2" v-else>
-                    <div class="card" style="min-height:150px">
-                        <div class="card-header">
-                            <div style="font-size: 20px;">
-                                Add New Project
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row no-gutters">
-                                <div class="col-auto">
-                                    <div class="cyan fill-height">&nbsp;</div>
-                                </div>
-                                <div class="col pa-3 py-4 black--text">
-                                    <h6 class="text-truncate text-uppercase">
-                                        Your awesome projects 🚀
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card-footer">
-                            <router-link class="btn btn-primary" to="/projects/new">Create New Projects</router-link>
-                        </div>
-
-                    </div>
-
+                <div class="col pa-3 py-4">
+                  <div>
+                    We can't offer more for free, please upgrade to get more.
+                  </div>
                 </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              <router-link class="btn btn-warning" to="upgrade"
+                >Upgrade Account</router-link
+              >
+            </div>
+          </div>
+        </div>
 
-                <div class="col-lg-6 col-sm-12 pb-2" v-for="(item, index) in projects.data" :key="index">
-
-                    <div class="card" style="min-height:150px">
-                        <div class="card-header">
-                            <div class="card-title d-flex mb-0">
-                                <span style="font-size: 20px;">
-
-                                    {{item.name.toUpperCase()}}
-                                </span>
-
-                                <router-link class="ml-auto btn btn-secondary"
-                                    :to="'/projects/'+item._id + '/settings'">
-                                    <b-icon-gear></b-icon-gear>
-                                </router-link>
-
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row no-gutters">
-                                <div class="col-auto">
-                                    <div class="cyan fill-height">&nbsp;</div>
-                                </div>
-
-                                <div class="col pa-3 py-4">
-                                    {{item.description}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-between">
-                                <router-link class="btn btn-primary" :to="'projects/'+item._id">Explore
-                                </router-link>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
+        <div class="col-lg-6 col-sm-12 pb-2" v-else>
+          <div class="card" style="min-height:150px">
+            <div class="card-header">
+              <div style="font-size: 20px;">
+                Add New Project
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <div class="cyan fill-height">&nbsp;</div>
                 </div>
-
+                <div class="col pa-3 py-4 black--text">
+                  <h6 class="text-truncate text-uppercase">
+                    Your awesome projects 🚀
+                  </h6>
+                </div>
+              </div>
             </div>
 
+            <div class="card-footer">
+              <router-link class="btn btn-primary" to="/projects/new"
+                >Create New Projects</router-link
+              >
+            </div>
+          </div>
         </div>
+
+        <div
+          class="col-lg-6 col-sm-12 pb-2"
+          v-for="(item, index) in projects.data"
+          :key="index"
+        >
+          <div class="card" style="min-height:150px">
+            <div class="card-header">
+              <div class="card-title d-flex mb-0text-uppercase">
+                <span style="font-size: 20px; ">
+                  {{ item.name }}
+                </span>
+
+                <router-link
+                  class="ml-auto btn btn-secondary"
+                  :to="'/projects/' + item._id + '/settings'"
+                >
+                  <b-icon-gear></b-icon-gear>
+                </router-link>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <div class="cyan fill-height">&nbsp;</div>
+                </div>
+
+                <div class="col pa-3 py-4">
+                  {{ item.description }}
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              <div class="d-flex justify-content-between">
+                <router-link
+                  class="btn btn-primary"
+                  :to="'projects/' + item._id"
+                  >Explore
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
-    export default {
-        name: "Projects",
-        computed: mapState(['projects']),
-        data() {
-            return {
-                loading: true,
-                dialog: true,
-            }
-        },
-        created: async function () {
-            this.$store.dispatch('getProjects').then(() => {
-                this.loading = false;
-            })
-        },
-    }
+export default {
+  name: "Projects",
+  computed: mapState(["projects"]),
+  data() {
+    return {
+      loading: true,
+      dialog: true,
+    };
+  },
+  created: async function() {
+    this.$store.dispatch("getProjects").then(() => {
+      this.loading = false;
+    });
+  },
+};
 </script>
--- src/views/dashboard/projects/single.vue
+++ src/views/dashboard/projects/single.vue
@@ -1,85 +1,88 @@
 <template>
-    <div>
+  <div>
+    <div class="container my-4">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <router-link to="/projects">Project</router-link>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">{{ id }}</li>
+        </ol>
+      </nav>
 
-        <div class="container my-4">
-
-
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <router-link to="/projects">Project</router-link>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">{{id}}</li>
-                </ol>
-            </nav>
-
-
-            <div class="card">
-                <div class="card-header d-flex">
-                    <h4>Project Details</h4>
-                    <div class="ml-auto" v-if="project._id">
-                        <router-link :to="project._id + '/edit'" class="btn btn-secondary white--text">
-                            <span class="d-none d-sm-flex">Edit</span>
-                        </router-link>
-                        <router-link class="btn btn-secondary ml-4 white--text" :to="project._id + '/settings'"
-                            outlined>
-                            <span class="d-none d-sm-flex">Settings</span>
-                        </router-link>
-                    </div>
-                </div>
-
-                <progress class="pure-material-progress-linear" v-if="loadingProject" />
-
-                <div class="card-body">
-                    <p class="body-1">
-                        Name : {{project.name}}
-                    </p>
-                    <p>
-                        ID : {{project._id}}
-                    </p>
-                    <p>
-                        Description : {{project.description}}
-                    </p>
-                    <p>
-                        Created On : {{  project.createdAt == null ? '' :new Date(project.createdAt).toLocaleString()}}
-                    </p>
-                </div>
-
-            </div>
-
-            <div class="my-4">
-                <div class="card-title d-flex">
-                    <h4>Endpoints</h4>
-                    <div class="ml-auto" v-if="project._id">
-                        <router-link :to="project._id + '/new'" class="btn btn-secondary">
-                            <span class="d-none d-sm-flex">
-                                Add Endpoint
-                            </span>
-
-                        </router-link>
-                    </div>
-                </div>
-            </div>
-
-            <progress class="pure-material-progress-linear" v-if="loadingProject" />
-
-            <div class="card mb-3" v-for="(endpoint,i) in project.endpoints" :key="i">
-                <div class="card-header">
-                    {{endpoint.name.toUpperCase()}}
-                </div>
-                <div class="card-body">
-                    Endpoint ID: {{ endpoint.endpoint_id }}
-                </div>
-                <div class="card-footer">
-                    <router-link class="btn btn-secondary" :to="'/projects/' + project._id +'/'+endpoint.endpoint_id">
-                        Explore
-                    </router-link>
-                </div>
-            </div>
-
+      <div class="card">
+        <div class="card-header d-flex">
+          <h4>Project Details</h4>
+          <div class="ml-auto" v-if="project._id">
+            <router-link
+              :to="project._id + '/edit'"
+              class="btn btn-secondary white--text"
+            >
+              <span class="d-none d-sm-flex">Edit</span>
+            </router-link>
+            <router-link
+              class="btn btn-secondary ml-4 white--text"
+              :to="project._id + '/settings'"
+              outlined
+            >
+              <span class="d-none d-sm-flex">Settings</span>
+            </router-link>
+          </div>
         </div>
 
-        <!-- <div class="text-center">
+        <progress class="pure-material-progress-linear" v-if="loadingProject" />
+
+        <div class="card-body">
+          <p class="body-1">Name : {{ project.name }}</p>
+          <p>ID : {{ project._id }}</p>
+          <p>Description : {{ project.description }}</p>
+          <p>
+            Created On :
+            {{
+              project.createdAt == null
+                ? ""
+                : new Date(project.createdAt).toLocaleString()
+            }}
+          </p>
+        </div>
+      </div>
+
+      <div class="my-4">
+        <div class="card-title d-flex">
+          <h4>Endpoints</h4>
+          <div class="ml-auto" v-if="project._id">
+            <router-link :to="project._id + '/new'" class="btn btn-secondary">
+              <span class="d-none d-sm-flex">
+                Add Endpoint
+              </span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+
+      <progress class="pure-material-progress-linear" v-if="loadingProject" />
+
+      <div
+        class="card mb-3"
+        v-for="(endpoint, i) in project.endpoints"
+        :key="i"
+      >
+        <div class="card-header text-uppercase">
+          {{ endpoint.name.toUpperCase() }}
+        </div>
+        <div class="card-body">Endpoint ID: {{ endpoint.endpoint_id }}</div>
+        <div class="card-footer">
+          <router-link
+            class="btn btn-secondary"
+            :to="'/projects/' + project._id + '/' + endpoint.endpoint_id"
+          >
+            Explore
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="text-center">
             <div class="text-center ma-2">
                 <v-dialog v-model="dialog" width="500">
 
@@ -121,63 +124,62 @@
                 </v-snackbar>
             </div>
         </div> -->
-    </div>
+  </div>
 </template>
 
 <script>
-    import { mapState } from 'vuex';
-    export default {
-        name: "Projects",
-        computed: mapState(['projects']),
-        data() {
-            return {
-                id: null,
-                loadingProject: false,
-                dialog: false,
-                timeout: 2000,
-                snackbar: false,
-                snackbarData: {
-                    text: "Updated",
-                    enable: true,
-                },
+import { mapState } from "vuex";
+export default {
+  name: "Projects",
+  computed: mapState(["projects"]),
+  data() {
+    return {
+      id: null,
+      loadingProject: false,
+      dialog: false,
+      timeout: 2000,
+      snackbar: false,
+      snackbarData: {
+        text: "Updated",
+        enable: true,
+      },
 
-                project: {
-                    name: null,
-                    description: null,
-                    _id: null,
-                    createOn: null,
-                    endpoints: [],
-                },
-                projectUpdate: {
-                    _id: null,
-                    name: null,
-                    description: null,
-                },
-                updateLoading: false,
-            }
-        },
-        created: async function () {
-            let _id = this.$route.params.id;
-            this.id = _id;
-            if (!this.projects.fetched) {
-                this.$store.dispatch('getProjects').then(e => {
-                    e.forEach(element => {
-                        if (element._id == _id) {
-                            this.loadingProject = false;
-                            this.project = element;
-                        }
-                    });
-                })
-            } else {
-                this.project = this.projects.data[_id];
-                this.loadingProject = false;
-            }
-
-            this.$store.dispatch('listEndpoint', _id).then(e => {
-                this.loadingEndpoint = false;
-                this.endpoints = (e.data);
-            });
-
-        },
+      project: {
+        name: null,
+        description: null,
+        _id: null,
+        createOn: null,
+        endpoints: [],
+      },
+      projectUpdate: {
+        _id: null,
+        name: null,
+        description: null,
+      },
+      updateLoading: false,
+    };
+  },
+  created: async function() {
+    let _id = this.$route.params.id;
+    this.id = _id;
+    if (!this.projects.fetched) {
+      this.$store.dispatch("getProjects").then((e) => {
+        e.forEach((element) => {
+          if (element._id == _id) {
+            this.loadingProject = false;
+            this.project = element;
+          }
+        });
+      });
+    } else {
+      this.project = this.projects.data[_id];
+      this.loadingProject = false;
     }
+
+    this.$store.dispatch("listEndpoint", _id).then((e) => {
+      this.loadingEndpoint = false;
+      this.endpoints = e.data;
+    });
+  },
+};
 </script>
